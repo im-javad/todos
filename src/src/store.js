d7c9eb8f2e8830f6772ad2aeae7457f34edbaee2
JavadJadidi <javadjadidi@gmail.com>
2023-04-21 16:45:09 +0430

    use-of-redux-dev-tools

--- src/src/store.js
+++ src/src/store.js
@@ -1,6 +1,9 @@
 import { legacy_createStore as createStore } from "redux";
 import appReducer from "./reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(appReducer);
+const composeEnhancers = composeWithDevTools();
+
+const store = createStore(appReducer , composeEnhancers);
 
 export default store;
